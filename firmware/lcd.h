e46a4f0927677ffbd937d94de10a5ac79e929d8b
Serialbocks <john@serialbocks.com>
2024-06-14 15:53:33 -0400

    Move LCD firmware to separate C file

---
+++ firmware/lcd.h
@@ -0,0 +1,16 @@
+#ifndef LCD_HEADER_H
+#define LCD_HEADER_H
+
+// Modes for lcd_send_byte
+#define LCD_CHARACTER  1
+#define LCD_COMMAND    0
+
+#define MAX_LINES      2
+#define MAX_CHARS      16
+
+void lcd_clear(void);
+void lcd_set_cursor(int line, int position);
+void lcd_string(const char *s);
+void lcd_init();
+
+#endif
