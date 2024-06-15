c6b0a96f412bf51b4d6134b47949b99f7f4e8589
Serialbocks <john@serialbocks.com>
2024-06-14 20:31:50 -0400

    Fix encoder driver

--- firmware/firmware.c
+++ firmware/firmware.c
@@ -16,7 +16,7 @@
 #include "quadrature_encoder.pio.h"
 
 int main() {
-    int new_value, delta, old_value;
+    int new_value, delta, old_value, display_value, old_display_value = 0;
     char message[MAX_CHARS];
 
     // Base pin to connect the A phase of the encoder.
@@ -32,9 +32,9 @@ int main() {
     // we don't really need to keep the offset, as this program must be loaded
     // at offset 0
     pio_add_program(pio, &quadrature_encoder_program);
-    quadrature_encoder_program_init(pio, sm, PIN_AB, 0);
+    quadrature_encoder_program_init(pio, sm, PIN_AB, 2);
 
-    snprintf(message, MAX_CHARS, "val: %d", new_value);
+    snprintf(message, MAX_CHARS, "val: %d", display_value);
     lcd_clear();
     lcd_write_line(0, message);
 
@@ -42,13 +42,14 @@ int main() {
         new_value = quadrature_encoder_get_count(pio, sm);
         delta = new_value - old_value;
         old_value = new_value;
-        if(delta != 0) {
-            snprintf(message, MAX_CHARS, "val: %d", -(old_value / 2));
+        display_value = -(old_value / 2);
+        if(display_value != old_display_value) {
+            snprintf(message, MAX_CHARS, "val: %d", display_value);
             lcd_clear();
             lcd_write_line(0, message);
         }
-
-        sleep_ms(10);
+        old_display_value = display_value;
+        //sleep_ms(100);
         
     }
 
