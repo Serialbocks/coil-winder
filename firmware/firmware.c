f9c4c14a3f0aedfecb0cafd8b9bde29b65ff743c
Serialbocks <john@serialbocks.com>
2024-06-17 10:27:31 -0400

    Hook up encoder pushbutton

--- firmware/firmware.c
+++ firmware/firmware.c
@@ -18,10 +18,12 @@
 int main() {
     int new_value, old_value = 0;
     char message[MAX_CHARS];
+    uint switch_new, switch_old = 0;
 
     // Base pin to connect the A phase of the encoder.
     // The B phase must be connected to the next pin
-    const uint PIN_AB = 10;
+    const uint AB_PIN = 10;
+    const uint SWITCH_PIN = 15;
 
     stdio_init_all();
 
@@ -32,7 +34,10 @@ int main() {
     // we don't really need to keep the offset, as this program must be loaded
     // at offset 0
     pio_add_program(pio, &quadrature_encoder_program);
-    quadrature_encoder_program_init(pio, sm, PIN_AB, 0);
+    quadrature_encoder_program_init(pio, sm, AB_PIN, 0);
+
+    gpio_init(SWITCH_PIN);
+    gpio_set_dir(SWITCH_PIN, GPIO_IN);
 
     //static char *message[] =
     //{
@@ -46,13 +51,22 @@ int main() {
     lcd_write_line(0, message);
 
     while (1) {
+        // Encoder Value
         new_value = quadrature_encoder_get_count(pio, sm);
-        if(old_value != new_value) {
-            snprintf(message, MAX_CHARS, "val: %d", new_value);
+        
+        // Switch Value
+        switch_new = gpio_get(SWITCH_PIN);
+        if(old_value != new_value || switch_old != switch_new) {
             lcd_clear();
+            snprintf(message, MAX_CHARS, "val: %d", new_value);
             lcd_write_line(0, message);
+            snprintf(message, MAX_CHARS, "switch: %u", switch_new);
+            lcd_write_line(1, message);
         }
+
         old_value = new_value;
+        switch_old = switch_new;
+
         sleep_ms(10);
         
     }
